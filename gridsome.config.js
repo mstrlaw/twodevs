ad3218a7a28b5756a95163ac7f6dc15a4297a095
Lawrence Almeida <lawbraun.almeida@gmail.com>
2023-10-28 12:54:38 +0100

    Project scaffolding 🚀

---
+++ gridsome.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteName: 'Two Devs Online',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`,
+        modulePath: `src/admin/index.js`
+      }
+    },
+  ]
+}
